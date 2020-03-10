f9ac9ca0b1ce322466ccd5bc0844292fa75a7617
Performance Media <szeszej@gmail.com>
2020-03-10 09:09:01 +0100

    added testing version to the main folder

---
+++ precache-manifest.299462f472de3c5a09bcb9f7d57d57ae.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "992fc191d7201cdfd87285c6846462a9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e6ddc20a527dab60f1ce",
+    "url": "/static/css/main.3e1d0090.chunk.css"
+  },
+  {
+    "revision": "768a8e7120f1f0dc1563",
+    "url": "/static/js/2.55cc58f7.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/static/js/2.55cc58f7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e6ddc20a527dab60f1ce",
+    "url": "/static/js/main.c7743aab.chunk.js"
+  },
+  {
+    "revision": "9c01e2f4133f737e5e4c",
+    "url": "/static/js/runtime-main.1f4f37e9.js"
+  }
+]);
