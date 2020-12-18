6e8426d224a74e7bda8741216e4c4c5c1e295a08
unknown <szeszej@gmail.com>
2020-12-18 11:44:34 +0100

    latest test version

---
+++ precache-manifest.d0e424578f67471c7d16f179cc9a67d7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "86ee44483deb47132c0d349964e551eb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a0ba3c538ca63656cc90",
+    "url": "/static/css/main.f835825a.chunk.css"
+  },
+  {
+    "revision": "8c65755d184c3ce540a5",
+    "url": "/static/js/2.d14c7743.chunk.js"
+  },
+  {
+    "revision": "2abe6ea1b5ca6a256d340a32b75ee5be",
+    "url": "/static/js/2.d14c7743.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a0ba3c538ca63656cc90",
+    "url": "/static/js/main.3e2d4f74.chunk.js"
+  },
+  {
+    "revision": "9c01e2f4133f737e5e4c",
+    "url": "/static/js/runtime-main.1f4f37e9.js"
+  }
+]);
