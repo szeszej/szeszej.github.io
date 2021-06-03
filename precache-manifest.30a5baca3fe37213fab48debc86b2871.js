0027cbc3fef6651c001cb069d6281740e08f5d62
unknown <>
2021-06-03 23:45:29 +0200

    kiedy ryc?

---
+++ precache-manifest.30a5baca3fe37213fab48debc86b2871.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "146ad1f129e8267c94d90f3daa1c136c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e11f6bbf54d6965d1076",
+    "url": "/static/css/main.76cb76a8.chunk.css"
+  },
+  {
+    "revision": "84706d02c1e021c88f53",
+    "url": "/static/js/2.c872bf67.chunk.js"
+  },
+  {
+    "revision": "2abe6ea1b5ca6a256d340a32b75ee5be",
+    "url": "/static/js/2.c872bf67.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f9be0c9fb601d5e5bda9",
+    "url": "/static/js/3.295b7986.chunk.js"
+  },
+  {
+    "revision": "f02901757fd8e471cfe6",
+    "url": "/static/js/4.533662f5.chunk.js"
+  },
+  {
+    "revision": "b63e9d36746e04ca6b24",
+    "url": "/static/js/5.d378f650.chunk.js"
+  },
+  {
+    "revision": "64b0a879e26d04c04d4b",
+    "url": "/static/js/6.83663b81.chunk.js"
+  },
+  {
+    "revision": "635db8e7b8a46032e32f",
+    "url": "/static/js/7.d639e9d9.chunk.js"
+  },
+  {
+    "revision": "e11f6bbf54d6965d1076",
+    "url": "/static/js/main.4afb0a31.chunk.js"
+  },
+  {
+    "revision": "57459b6f6c843e138b7d",
+    "url": "/static/js/runtime-main.fa656e43.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.88c17bea1d46ac278cfb568e05e6b10f.js"
+  "/precache-manifest.30a5baca3fe37213fab48debc86b2871.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
